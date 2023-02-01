0e4104dec518fc4813e804467890e528d44dd1b7
Danil <shwedx@Yandex.ru>
2023-02-01 14:49:27 +0300

    3 task done

---
+++ HomeWork3/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка)
+
+Console.Write("Введите число > ");
+string ournumber = Console.ReadLine();
+int First = Convert.ToInt32(ournumber);
+
+
+if(First % 2 == 0)
+{
+    Console.WriteLine("Your number allowed");
+}
+else
+{
+    Console.WriteLine("Your number is not allowed");
+}
